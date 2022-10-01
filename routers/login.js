d501c47a8d105f726e570eaa5151166f60755764
YbiG-blog <yash2010146@akgec.ac.in>
2022-10-01 20:19:11 +0530

    error in cookie

--- routers/login.js
+++ routers/login.js
@@ -3,8 +3,8 @@ const express = require("express");
 const router = new express.Router();
 const jwt = require("jsonwebtoken");
 
-router.get("/login", function(req, res) {
-    res.render("login");
+router.get("/login", function (req, res) {
+  res.render("login");
 });
 
 router.post("/login", ({ body }, res) => {
@@ -13,25 +13,24 @@ router.post("/login", ({ body }, res) => {
     if (adminId === process.env.ADMINID) {
       const pay_load = { _id: process.env.ADMINID };
       const cookie_token = jwt.sign(pay_load, process.env.TOKEN_SECRET_KEY);
-            //add cookie
-            res.cookie("jwt_stickman", cookie_token, {
-              secure: true,
-              expires: new Date(Date.now() + 86400000),
-              httpOnly: true,
-            });
       if (password === process.env.ADMINPASS) {
+        //add cookie
+        res.cookie("jwt_stickman", cookie_token, {
+          secure: true,
+          expires: new Date(Date.now() + 86400000),
+          httpOnly: true,
+        });
         res.redirect("/api/user/admin");
         return res.status(200).json({
-          status: 'Login successful!',
+          status: "Login successful!",
           success: true,
-          cookie_token: cookie_token
+          cookie_token: cookie_token,
         });
-      }
-      else{
-        res.send({ msg:"Wrong Password"});
+      } else {
+        res.send({ msg: "Wrong Password" });
       }
     } else {
-      res.send({ msg:"!Invalid details"});
+      res.send({ msg: "!Invalid details" });
     }
   } catch (err) {
     res.status(400).send(`err ${err}`);
